c5e59ec52d55c7f6331ab14a3e7176d183167889
lionelbarrow <lionelbarrow@gmail.com>
2013-05-26 17:29:54 -0500

    Move stub gateways to test_values.go

--- gateway.go
+++ gateway.go
@@ -56,23 +56,3 @@ func (this BraintreeGateway) Execute(method, urlExtension string, body io.Reader
 
 	return &Response{StatusCode: response.StatusCode, Status: response.Status, Body: contents}, nil
 }
-
-// Stub gateways, included for testing
-type blowUpGateway struct{}
-
-func (this blowUpGateway) Execute(method, url string, body io.Reader) (*Response, error) {
-	return &Response{StatusCode: 500, Status: "500 Internal Server Error"}, nil
-}
-
-type badInputGateway struct{}
-
-func (this badInputGateway) Execute(method, url string, body io.Reader) (*Response, error) {
-	xml := "<?xml version=\"1.0\" encoding=\"UTF-8\"?><api-error-response><errors><errors type=\"array\"/></errors><message>Card Issuer Declined CVV</message></api-error-response>"
-	return &Response{StatusCode: 422, Body: []byte(xml)}, nil
-}
-
-type notFoundGateway struct{}
-
-func (this notFoundGateway) Execute(method, url string, body io.Reader) (*Response, error) {
-	return &Response{StatusCode: 404}, nil
-}
--- test_values.go
+++ test_values.go
@@ -1,5 +1,7 @@
 package braintree
 
+import "io"
+
 var (
 	TestCreditCards = map[string]CreditCard{"visa": CreditCard{Number: "4111111111111111"}}
 
@@ -14,3 +16,22 @@ var (
 	txGateway       = TransactionGateway{baseGateway}
 	customerGateway = CustomerGateway{baseGateway}
 )
+
+type blowUpGateway struct{}
+
+func (this blowUpGateway) Execute(method, url string, body io.Reader) (*Response, error) {
+	return &Response{StatusCode: 500, Status: "500 Internal Server Error"}, nil
+}
+
+type badInputGateway struct{}
+
+func (this badInputGateway) Execute(method, url string, body io.Reader) (*Response, error) {
+	xml := "<?xml version=\"1.0\" encoding=\"UTF-8\"?><api-error-response><errors><errors type=\"array\"/></errors><message>Card Issuer Declined CVV</message></api-error-response>"
+	return &Response{StatusCode: 422, Body: []byte(xml)}, nil
+}
+
+type notFoundGateway struct{}
+
+func (this notFoundGateway) Execute(method, url string, body io.Reader) (*Response, error) {
+	return &Response{StatusCode: 404}, nil
+}
