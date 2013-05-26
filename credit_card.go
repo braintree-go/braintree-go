dd9bc2b5d15d9df48595d9114bdc970848094f17
lionelbarrow <lionelbarrow@gmail.com>
2013-05-26 17:55:40 -0500

    Add test for when a customer create fails

--- credit_card.go
+++ credit_card.go
@@ -1,6 +1,12 @@
 package braintree
 
 type CreditCard struct {
-	Number         string `xml:"number"`
-	ExpirationDate string `xml:"expiration-date"`
+	Number         string             `xml:"number"`
+	ExpirationDate string             `xml:"expiration-date"`
+	CVV            string             `xml:"cvv,omitempty"`
+	Options        *CreditCardOptions `xml:"options,omitempty"`
+}
+
+type CreditCardOptions struct {
+	VerifyCard bool `xml:"verify-card,omitempty"`
 }
--- customer_integration_test.go
+++ customer_integration_test.go
@@ -29,3 +29,34 @@ func TestCustomerCreate(t *testing.T) {
 		t.Errorf("Customer did not receive an ID")
 	}
 }
+
+/* This test will fail unless the account under test has CVV rules set up in the
+Braintree gateway. See https://www.braintreepayments.com/docs/ruby/card_verifications/overview 
+for more details. */
+func TestCustomerCreateWithErrors(t *testing.T) {
+	customer := Customer{
+		FirstName: "Lionel",
+		LastName:  "Barrow",
+		Company:   "Braintree",
+		Email:     "lionel.barrow@example.com",
+		Phone:     "312.555.1234",
+		Fax:       "614.555.5678",
+		Website:   "http://www.example.com",
+		CreditCard: &CreditCard{
+			Number:         TestCreditCards["visa"].Number,
+			ExpirationDate: "05/14",
+			CVV:            "200",
+			Options: &CreditCardOptions{
+				VerifyCard: true,
+			},
+		},
+	}
+
+	result, err := customerGateway.Create(customer)
+
+	if err != nil {
+		t.Errorf(err.Error())
+	} else if result.Success() {
+		t.Errorf("Customer created succeeded with bad CVV and verify card true")
+	}
+}
