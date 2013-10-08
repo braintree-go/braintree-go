bccce64817f4c88fc99d7287c0cf8dffcbe6d334
Erik Aigner <erik@chocomoko.com>
2013-10-08 22:14:59 +0200

    Create and update time fields

--- transaction.go
+++ transaction.go
@@ -15,7 +15,79 @@ type Transaction struct {
 	BillingAddress     *Address            `xml:"billing,omitempty"`
 	ShippingAddress    *Address            `xml:"shipping,omitempty"`
 	Options            *TransactionOptions `xml:"options,omitempty"`
+	CreatedAt          string              `xml:"created-at,omitempty"`
+	UpdatedAt          string              `xml:"updated-at,omitempty"`
 }
+
+// TODO: not all transaction fields are implemented yet, here are the missing fields (add on demand)
+//
+// <transaction>
+//   <currency-iso-code>USD</currency-iso-code>
+//   <refund-id nil="true"></refund-id>
+//   <refund-ids type="array"/>
+//   <refunded-transaction-id nil="true"></refunded-transaction-id>
+//   <settlement-batch-id>2013-10-08_49grybq7pbtsnvsr</settlement-batch-id>
+//   <custom-fields>
+//   </custom-fields>
+//   <avs-error-response-code nil="true"></avs-error-response-code>
+//   <avs-postal-code-response-code>I</avs-postal-code-response-code>
+//   <avs-street-address-response-code>I</avs-street-address-response-code>
+//   <cvv-response-code>I</cvv-response-code>
+//   <gateway-rejection-reason nil="true"></gateway-rejection-reason>
+//   <processor-authorization-code>YCSBWR</processor-authorization-code>
+//   <processor-response-code>1000</processor-response-code>
+//   <processor-response-text>Approved</processor-response-text>
+//   <voice-referral-number nil="true"></voice-referral-number>
+//   <purchase-order-number nil="true"></purchase-order-number>
+//   <tax-amount nil="true"></tax-amount>
+//   <tax-exempt type="boolean">false</tax-exempt>
+//   <status-history type="array">
+//     <status-event>
+//       <timestamp type="datetime">2013-10-07T17:26:14Z</timestamp>
+//       <status>authorized</status>
+//       <amount>7.00</amount>
+//       <user>eaigner</user>
+//       <transaction-source>Recurring</transaction-source>
+//     </status-event>
+//     <status-event>
+//       <timestamp type="datetime">2013-10-07T17:26:14Z</timestamp>
+//       <status>submitted_for_settlement</status>
+//       <amount>7.00</amount>
+//       <user>eaigner</user>
+//       <transaction-source>Recurring</transaction-source>
+//     </status-event>
+//     <status-event>
+//       <timestamp type="datetime">2013-10-08T07:06:38Z</timestamp>
+//       <status>settled</status>
+//       <amount>7.00</amount>
+//       <user nil="true"></user>
+//       <transaction-source></transaction-source>
+//     </status-event>
+//   </status-history>
+//   <plan-id>bronze</plan-id>
+//   <subscription-id>jqsydb</subscription-id>
+//   <subscription>
+//     <billing-period-end-date type="date">2013-11-06</billing-period-end-date>
+//     <billing-period-start-date type="date">2013-10-07</billing-period-start-date>
+//   </subscription>
+//   <add-ons type="array"/>
+//   <discounts type="array"/>
+//   <descriptor>
+//     <name nil="true"></name>
+//     <phone nil="true"></phone>
+//   </descriptor>
+//   <recurring type="boolean">true</recurring>
+//   <channel nil="true"></channel>
+//   <service-fee-amount nil="true"></service-fee-amount>
+//   <escrow-status nil="true"></escrow-status>
+//   <disbursement-details>
+//     <disbursement-date type="date">2013-10-08</disbursement-date>
+//     <settlement-amount>7.00</settlement-amount>
+//     <settlement-currency-iso-code>USD</settlement-currency-iso-code>
+//     <settlement-currency-exchange-rate>1</settlement-currency-exchange-rate>
+//     <funds-held type="boolean">false</funds-held>
+//   </disbursement-details>
+// </transaction>
 
 type Transactions struct {
 	Transaction []*Transaction `xml:"transaction"`
