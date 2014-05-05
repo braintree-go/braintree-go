377670cc49199fd179e5ddb78c8a4f934100c4c4
Jonathan Azoff <jon@azoffdesign.com>
2014-05-05 14:23:19 -0700

    adds json tags for json encoding

--- merchant_account.go
+++ merchant_account.go
@@ -1,39 +1,39 @@
 package braintree
 
 type MerchantAccount struct {
-	XMLName                 string                         `xml:"merchant-account,omitempty"`
-	Id                      string                         `xml:"id,omitempty"`
-	MasterMerchantAccountId string                         `xml:"master-merchant-account-id,omitempty"`
-	TOSAccepted             bool                           `xml:"tos_accepted,omitempty"`
-	Individual              *MerchantAccountPerson         `xml:"individual,omitempty"`
-	Business                *MerchantAccountBusiness       `xml:"business,omitempty"`
-	FundingOptions          *MerchantAccountFundingOptions `xml:"funding,omitempty"`
-	Status                  string                         `xml:"status,omitempty"`
+	XMLName                 string                         `xml:"merchant-account,omitempty" json:"-"`
+	Id                      string                         `xml:"id,omitempty" json:"id"`
+	MasterMerchantAccountId string                         `xml:"master-merchant-account-id,omitempty" json:"-"`
+	TOSAccepted             bool                           `xml:"tos_accepted,omitempty" json: "-"`
+	Individual              *MerchantAccountPerson         `xml:"individual,omitempty" json:"individual"`
+	Business                *MerchantAccountBusiness       `xml:"business,omitempty" json:"business"`
+	FundingOptions          *MerchantAccountFundingOptions `xml:"funding,omitempty" json:"funding"`
+	Status                  string                         `xml:"status,omitempty"  json:"status,omitempty"`
 }
 
 type MerchantAccountPerson struct {
-	FirstName   string   `xml:"first-name,omitempty"`
-	LastName    string   `xml:"last-name,omitempty"`
-	Email       string   `xml:"email,omitempty"`
-	Phone       string   `xml:"phone,omitempty"`
-	DateOfBirth string   `xml:"date-of-birth,omitempty"`
-	SSN         string   `xml:"ssn,omitempty"`
-	Address     *Address `xml:"address,omitempty"`
+	FirstName   string   `xml:"first-name,omitempty" json:"first_name,omitempty"`
+	LastName    string   `xml:"last-name,omitempty" json:"last_name,omitempty"`
+	Email       string   `xml:"email,omitempty" json:"email,omitempty"`
+	Phone       string   `xml:"phone,omitempty" json:"phone,omitempty"`
+	DateOfBirth string   `xml:"date-of-birth,omitempty" json:"dob,omitempty"`
+	SSN         string   `xml:"ssn,omitempty" json:"ssn,omitempty"`
+	Address     *Address `xml:"address,omitempty" json:"address,omitempty"`
 }
 
 type MerchantAccountBusiness struct {
-	LegalName string   `xml:"legal-name,omitempty"`
-	DbaName   string   `xml:"dba-name,omitempty"`
-	TaxId     string   `xml:"tax-id,omitempty"`
-	Address   *Address `xml:"address,omitempty"`
+	LegalName string   `xml:"legal-name,omitempty" json:"legal_name,omitempty"`
+	DbaName   string   `xml:"dba-name,omitempty" json:"dba_name,omitempty"`
+	TaxId     string   `xml:"tax-id,omitempty" json:"tax_id,omitempty"`
+	Address   *Address `xml:"address,omitempty" json:"address,omitempty"`
 }
 
 type MerchantAccountFundingOptions struct {
-	Destination   string `xml:"destination,omitempty"`
-	Email         string `xml:"email,omitempty"`
-	MobilePhone   string `xml:"mobile-phone,omitempty"`
-	AccountNumber string `xml:"account-number,omitempty"`
-	RoutingNumber string `xml:"routing-number,omitempty"`
+	Destination   string `xml:"destination,omitempty" json:"destination,omitempty"`
+	Email         string `xml:"email,omitempty" json:"email,omitempty"`
+	MobilePhone   string `xml:"mobile-phone,omitempty" json:"mobile_phone,omitempty"`
+	AccountNumber string `xml:"account-number,omitempty" json:"account_number,omitempty"`
+	RoutingNumber string `xml:"routing-number,omitempty" json:"routing_number,omitempty"`
 }
 
 const (
