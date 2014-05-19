622ff921d9ddbc5445f937c30f76bda5d764eb01
Michael S. Manley <msm@msmprojects.com>
2014-05-19 12:15:06 -0500

    Additional options for the txstatus test tool.

--- examples/txstatus/txstatus.go
+++ examples/txstatus/txstatus.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strings"
 
 	"github.com/publicgoodsw/braintree-go"
 )
@@ -12,8 +13,8 @@ import (
 func main() {
 	flag.Parse()
 
-	if flag.NArg() < 1 {
-		log.Fatalln("must provide transaction id")
+	if flag.NArg() < 2 {
+		log.Fatalln("USAGE: txstatus [command] txid")
 	}
 
 	var gateway = braintree.New(
@@ -23,15 +24,71 @@ func main() {
 		os.Getenv("BRAINTREE_PRIV_KEY"),
 	)
 
-	txid := flag.Arg(0)
-	fmt.Println(txid)
+	var showstatus bool
 
+	cmd := strings.ToUpper(flag.Arg(0))
+	txid := flag.Arg(1)
+	fmt.Println("Transaction: " + txid)
 	tx, err := gateway.Transaction().Find(txid)
-
-	if err == nil {
-		fmt.Println("Status: " + tx.Status)
-		fmt.Println("Escrow Status: " + tx.EscrowStatus)
+	if tx == nil {
+		fmt.Println("Transanction not found.")
 	} else {
-		fmt.Println(err)
+		fmt.Println("Transaction found.")
+	}
+
+	fmt.Println(cmd)
+
+	switch cmd {
+	case "STATUS":
+		showstatus = true
+
+	case "SUBMIT":
+		showstatus = true
+		if tx.Status == braintree.TxAuthorized {
+			tx, err = gateway.Transaction().SubmitForSettlement(txid)
+		} else {
+			fmt.Println("Cannot settle transaction until transaction is authorized.")
+		}
+
+	case "HOLD":
+		showstatus = true
+		if (tx.Status == braintree.TxSubmittedForSettlement) || (tx.Status == braintree.TxAuthorized) {
+			tx, err = gateway.Transaction().HoldInEscrow(txid)
+		} else {
+			fmt.Println("Cannot release transaction until transaction is authorized or submitted for settlement.")
+		}
+
+	case "RELEASE":
+		showstatus = true
+		if tx.EscrowStatus == braintree.TxEscrowHeld {
+			tx, err = gateway.Transaction().ReleaseFromEscrow(txid)
+		} else {
+			fmt.Println("Cannot release transaction until held for escrow.")
+		}
+
+	case "CANCELRELEASE":
+		showstatus = true
+		if tx.EscrowStatus == braintree.TxEscrowReleasePending {
+			tx, err = gateway.Transaction().CancelRelease(txid)
+		} else {
+			fmt.Println("Cannot cancel release until release is pending.")
+		}
+
+	case "VOID":
+		showstatus = true
+		if (tx.Status == braintree.TxSubmittedForSettlement) || (tx.Status == braintree.TxAuthorized) {
+			tx, err = gateway.Transaction().Void(txid)
+		} else {
+			fmt.Println("Cannot void transaction until transaction is authorized or submitted for settlement.")
+		}
+	}
+
+	if showstatus {
+		if err == nil {
+			fmt.Println("Status: " + tx.Status)
+			fmt.Println("Escrow Status: " + tx.EscrowStatus)
+		} else {
+			fmt.Println(err)
+		}
 	}
 }
