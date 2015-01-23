3198b9e3f3afc69dba14d079899684b73d951004
jszwedko <jesse.szwedko@gmail.com>
2015-01-23 14:13:45 -0800

    Add Date.MarshalXML for parity
    Though it seems like the gateway accepts full timestamps.
    
    Also embedded a Time rather than a *Time struct since it seems like Time
    prefers to be a value (given the method signatures).

--- date.go
+++ date.go
@@ -8,9 +8,11 @@ import (
 // Date wraps a time object but handles deserializing dates returned from the Braintree API
 // e.g. "2014-02-09"
 type Date struct {
-	*time.Time
+	time.Time
 }
 
+// UnmarshalXML interprets Braintree's date format from XML to initialize the Date
+// e.g. "2014-02-09"
 func (d *Date) UnmarshalXML(dec *xml.Decoder, start xml.StartElement) error {
 	var v string
 	dec.DecodeElement(&v, &start)
@@ -20,6 +22,12 @@ func (d *Date) UnmarshalXML(dec *xml.Decoder, start xml.StartElement) error {
 		return err
 	}
 
-	*d = Date{Time: &parse}
+	*d = Date{Time: parse}
 	return nil
+}
+
+// MarshalXML Outputs the date in a format the Braintree expects to see date
+// e.g. "2014-02-09"
+func (d *Date) MarshalXML(e *xml.Encoder, start xml.StartElement) error {
+	return e.EncodeElement(d.Format("2006-01-02"), start)
 }
--- date_test.go
+++ date_test.go
@@ -3,6 +3,7 @@ package braintree
 import (
 	"encoding/xml"
 	"testing"
+	"time"
 )
 
 func TestDateUnmarshalXML(t *testing.T) {
@@ -15,5 +16,19 @@ func TestDateUnmarshalXML(t *testing.T) {
 
 	if date.Format("2006-01-02") != "2014-02-09" {
 		t.Fatalf("expected 2014-02-09 got %s", date)
+	}
+}
+
+func TestDateMarshalXML(t *testing.T) {
+	date := &Date{Time: time.Date(2014, 2, 9, 0, 0, 0, 0, time.Local)}
+	expected := `<Date>2014-02-09</Date>`
+
+	b, err := xml.Marshal(date)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if string(b) != expected {
+		t.Fatalf("expected %s got %s", expected, string(b))
 	}
 }
