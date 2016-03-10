492f9bd1ff15b6e38a9b98d921c519f9c86b2825
Valentin Hăloiu <vially.ichb@gmail.com>
2016-03-10 02:51:55 +0200

    Implement additional payment gateway operations

--- payment_method_integration_test.go
+++ payment_method_integration_test.go
@@ -1,18 +1,24 @@
 package braintree
 
-import "testing"
+import (
+	"fmt"
+	"math/rand"
+	"testing"
+	"time"
+)
 
-func TestCreatePaymentMethod(t *testing.T) {
+func TestPaymentMethod(t *testing.T) {
 	cust, err := testGateway.Customer().Create(&Customer{})
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	nonce := FakeNonceTransactable
+	g := testGateway.PaymentMethod()
 
-	paymentMethod, err := testGateway.PaymentMethod().Create(&PaymentMethodRequest{
+	// Create using credit card
+	paymentMethod, err := g.Create(&PaymentMethodRequest{
 		CustomerId:         cust.Id,
-		PaymentMethodNonce: nonce,
+		PaymentMethodNonce: FakeNonceTransactableVisa,
 	})
 	if err != nil {
 		t.Fatal(err)
@@ -23,5 +29,73 @@ func TestCreatePaymentMethod(t *testing.T) {
 	}
 	if paymentMethod.GetToken() == "" {
 		t.Errorf("Got paymentMethod token %#v, want a value", paymentMethod.GetToken())
+	}
+
+	// Update using different credit card
+	rand.Seed(time.Now().UTC().UnixNano())
+	token := fmt.Sprintf("btgo_test_token_%d", rand.Int()+1)
+	paymentMethod, err = g.Update(paymentMethod.GetToken(), &PaymentMethodRequest{
+		PaymentMethodNonce: FakeNonceTransactableMasterCard,
+		Token:              token,
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if paymentMethod.GetToken() != token {
+		t.Errorf("Got paymentMethod token %#v, want %#v", paymentMethod.GetToken(), token)
+	}
+
+	// Updating with different payment method type should fail
+	if _, err = g.Update(token, &PaymentMethodRequest{PaymentMethodNonce: FakeNoncePayPalFuturePayment}); err == nil {
+		t.Errorf("Updating with a different payment method type should have failed")
+	}
+
+	// Find credit card
+	paymentMethod, err = g.Find(token)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if paymentMethod.GetCustomerId() != cust.Id {
+		t.Errorf("Got paymentMethod customer Id %#v, want %#v", paymentMethod.GetCustomerId(), cust.Id)
+	}
+	if paymentMethod.GetToken() != token {
+		t.Errorf("Got paymentMethod token %#v, want %#v", paymentMethod.GetToken(), token)
+	}
+
+	// Delete credit card
+	if err := g.Delete(token); err != nil {
+		t.Fatal(err)
+	}
+
+	// Create using PayPal
+	paymentMethod, err = g.Create(&PaymentMethodRequest{
+		CustomerId:         cust.Id,
+		PaymentMethodNonce: FakeNoncePayPalFuturePayment,
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Find PayPal
+	_, err = g.Find(paymentMethod.GetToken())
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Updating a PayPal account with a different payment method nonce of any kind should fail
+	if _, err = g.Update(paymentMethod.GetToken(), &PaymentMethodRequest{PaymentMethodNonce: FakeNoncePayPalOneTimePayment}); err == nil {
+		t.Errorf("Updating a PayPal account with a different nonce should have failed")
+	}
+
+	// Delete PayPal
+	if err := g.Delete(paymentMethod.GetToken()); err != nil {
+		t.Fatal(err)
+	}
+
+	// Cleanup
+	if err := testGateway.Customer().Delete(cust.Id); err != nil {
+		t.Fatal(err)
 	}
 }
