e60b56deedbbc018c4b5aba423328b97c888cc22
Valentin Hăloiu <vially.ichb@gmail.com>
2016-03-29 05:01:23 +0300

    Add payment method options struct

--- payment_method.go
+++ payment_method.go
@@ -1,7 +1,15 @@
 package braintree
 
 type PaymentMethod struct {
-	CustomerId         string `xml:"customer-id,omitempty"`
-	Token              string `xml:"token,omitempty"`
-	PaymentMethodNonce string `xml:"payment-method-nonce,omitempty"`
+	CustomerId         string                `xml:"customer-id,omitempty"`
+	Token              string                `xml:"token,omitempty"`
+	PaymentMethodNonce string                `xml:"payment-method-nonce,omitempty"`
+	Options            *PaymentMethodOptions `xml:"options,omitempty"`
+}
+
+type PaymentMethodOptions struct {
+	VerifyCard                    bool   `xml:"verify-card,omitempty"`
+	MakeDefault                   bool   `xml:"make-default,omitempty"`
+	FailOnDuplicatePaymentMethod  bool   `xml:"fail-on-duplicate-payment-method,omitempty"`
+	VerificationMerchantAccountId string `xml:"verification-merchant-account-id,omitempty"`
 }
