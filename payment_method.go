800e8440667a578debd43a8bdf8595b3f6c329db
Brian Powell <brian.powell@completeset.com>
2016-11-09 10:39:27 -0500

    updating camelized keys o payment methods

--- payment_method.go
+++ payment_method.go
@@ -1,28 +1,29 @@
 package braintree
 
 type PaymentMethod struct {
-	CustomerId         string                `xml:"customer-id,omitempty" json:"customerId,omitempty" bson:"customerId,omitempty"`
+	UserId             string                `xml:"user-id,omitempty" json:"user_id,omitempty" bson:"user_id,omitempty"`
+	CustomerId         string                `xml:"customer-id,omitempty" json:"customer_id,omitempty" bson:"customer_id,omitempty"`
 	Token              string                `xml:"token,omitempty" json:"token,omitempty" bson:"token,omitempty"`
-	PaymentMethodNonce string                `xml:"payment-method-nonce,omitempty" json:"paymentMethodNonce,omitempty" bson:"paymentMethodNonce,omitempty"`
+	PaymentMethodNonce string                `xml:"payment-method-nonce,omitempty" json:"payment_method_nonce,omitempty" bson:"payment_method_nonce,omitempty"`
 	Number             string                `xml:"number,omitempty" json:"number,omitempty" bson:"number,omitempty"`
-	ExpirationDate     string                `xml:"expiration-date,omitempty" json:"expirationDate,omitempty" bson:"expirationDate,omitempty"`
-	ExpirationMonth    string                `xml:"expiration-month,omitempty" json:"expirationMonth,omitempty" bson:"expirationMonth,omitempty"`
-	ExpirationYear     string                `xml:"expiration-year,omitempty" json:"expirationYear,omitempty" bson:"expirationYear,omitempty"`
+	ExpirationDate     string                `xml:"expiration-date,omitempty" json:"expiration_date,omitempty" bson:"expiration_date,omitempty"`
+	ExpirationMonth    string                `xml:"expiration-month,omitempty" json:"expiration_month,omitempty" bson:"expiration_month,omitempty"`
+	ExpirationYear     string                `xml:"expiration-year,omitempty" json:"expiration_year,omitempty" bson:"expiration_year,omitempty"`
 	CVV                string                `xml:"cvv,omitempty" json:"cvv,omitempty" bson:"cvv,omitempty"`
 	Options            *PaymentMethodOptions `xml:"options,omitempty" json:"options,omitempty" bson:"options,omitempty"`
-	CardholderName     string                `xml:"cardholder-name,omitempty" json:"cardholderName,omitempty" bson:"cardholderName,omitempty"`
-	DeviceData         string                `xml:"device-data,omitempty" json:"deviceData,omitempty" bson:"deviceData,omitempty"`
-	BillingId          string                `xml:"billing-id,omitempty" json:"billingId,omitempty" bson:"billingId,omitempty"`
-	BillingAddress     *Address              `xml:"billing-address,omitempty" json:"billingAddress,omitempty" bson:"billingAddress,omitempty"`
+	CardholderName     string                `xml:"cardholder-name,omitempty" json:"cardholder_name,omitempty" bson:"cardholder_name,omitempty"`
+	DeviceData         string                `xml:"device-data,omitempty" json:"device_data,omitempty" bson:"device_data,omitempty"`
+	BillingId          string                `xml:"billing-id,omitempty" json:"billing_id,omitempty" bson:"billing_id,omitempty"`
+	BillingAddress     *Address              `xml:"billing-address,omitempty" json:"billing_address,omitempty" bson:"billing_address,omitempty"`
 }
 
 type PaymentMethodOptions struct {
-	VerifyCard                    bool   `xml:"verify-card,omitempty" json:"verifyCard,omitempty" bson:"verifyCard,omitempty"`
-	MakeDefault                   bool   `xml:"make-default,omitempty" json:"makeDefault,omitempty" bson:"makeDefault,omitempty"`
-	FailOnDuplicatePaymentMethod  bool   `xml:"fail-on-duplicate-payment-method,omitempty" json:"failOnDuplicatePaymentMethod,omitempty" bson:"failOnDuplicatePaymentMethod,omitempty"`
-	VerificationMerchantAccountId string `xml:"verification-merchant-account-id,omitempty" json:"verificationMerchantAccountId,omitempty" bson:"verificationMerchantAccountId,omitempty"`
+	VerifyCard                    bool   `xml:"verify-card,omitempty" json:"verify_card,omitempty" bson:"verify_card,omitempty"`
+	MakeDefault                   bool   `xml:"make-default,omitempty" json:"make_default,omitempty" bson:"make_default,omitempty"`
+	FailOnDuplicatePaymentMethod  bool   `xml:"fail-on-duplicate-payment-method,omitempty" json:"fail_on_duplicate_payment_method,omitempty" bson:"fail_on_duplicate_payment_method,omitempty"`
+	VerificationMerchantAccountId string `xml:"verification-merchant-account-id,omitempty" json:"verification_merchant_account_id,omitempty" bson:"verification_merchant_account_id,omitempty"`
 }
 
 type PaymentMethods struct {
-	PaymentMethod []*PaymentMethod `xml:"payment-method" json:"paymentMethod" bson:"paymentMethod"`
+	PaymentMethod []*PaymentMethod `xml:"payment-method" json:"payment_method" bson:"payment_method"`
 }
