9bcffc9783f4b4f896d5cb31b83810b3dcb4a5b6
Leigh McCulloch <leigh@mcchouse.com>
2017-03-04 01:13:07 +0000

    Add a test for using PayPalAccounts with Customers

--- customer_integration_test.go
+++ customer_integration_test.go
@@ -1,6 +1,7 @@
 package braintree
 
 import (
+	"reflect"
 	"testing"
 )
 
@@ -108,5 +109,35 @@ func TestCustomer(t *testing.T) {
 	}
 	if c4 != nil {
 		t.Fatal(c4)
+	}
+}
+
+func TestCustomerPayPalAccount(t *testing.T) {
+	customer, err := testGateway.Customer().Create(&Customer{})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	nonce := FakeNoncePayPalFuturePayment
+
+	paymentMethod, err := testGateway.PaymentMethod().Create(&PaymentMethodRequest{
+		CustomerId:         customer.Id,
+		PaymentMethodNonce: nonce,
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+	paypalAccount := paymentMethod.(*PayPalAccount)
+
+	customerFound, err := testGateway.Customer().Find(customer.Id)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if customerFound.PayPalAccounts == nil || len(customerFound.PayPalAccounts.PayPalAccount) != 1 {
+		t.Fatalf("Customer %#v expected to have one PayPalAccount", customerFound)
+	}
+	if !reflect.DeepEqual(customerFound.PayPalAccounts.PayPalAccount[0], paypalAccount) {
+		t.Fatalf("Got Customer %#v PayPalAccount %#v, want %#v", customerFound, customerFound.PayPalAccounts.PayPalAccount[0], paypalAccount)
 	}
 }
