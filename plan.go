3a5393cf91a2d7f81bf366cb595be33230a7a837
Leigh McCulloch <leigh@mcchouse.com>
2017-03-09 06:25:26 +0000

    Add the xml names for AddOns and Discounts in Plan

--- plan.go
+++ plan.go
@@ -1,20 +1,21 @@
 package braintree
 
 import (
-	"github.com/lionelbarrow/braintree-go/nullable"
 	"time"
+
+	"github.com/lionelbarrow/braintree-go/nullable"
 )
 
 type Plan struct {
-	XMLName               string `xml:"plan"`
-	Id                    string `xml:"id"`
-	AddOns                AddOnList
+	XMLName               string              `xml:"plan"`
+	Id                    string              `xml:"id"`
+	AddOns                AddOnList           `xml:"add-ons"`
 	MerchantId            string              `xml:"merchant-id"`
 	BillingDayOfMonth     *nullable.NullInt64 `xml:"billing-day-of-month"`
 	BillingFrequency      *nullable.NullInt64 `xml:"billing-frequency"`
 	CurrencyISOCode       string              `xml:"currency-iso-code"`
 	Description           string              `xml:"description"`
-	Discounts             DiscountList
+	Discounts             DiscountList        `xml:"discounts"`
 	Name                  string              `xml:"name"`
 	NumberOfBillingCycles *nullable.NullInt64 `xml:"number-of-billing-cycles"`
 	Price                 *Decimal            `xml:"price"`
