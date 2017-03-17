9ef94f65393356c8b73921045ab890b5289817ea
Leigh McCulloch <leigh@mcchouse.com>
2017-03-17 18:01:46 +0000

    Peel back the changes being merged in to SubcriptionRequest only

--- modification.go
+++ modification.go
@@ -1,10 +1,6 @@
 package braintree
 
-import (
-	"time"
-
-	"github.com/lionelbarrow/braintree-go/nullable"
-)
+import "time"
 
 const (
 	ModificationKindDiscount = "discount"
@@ -20,36 +16,4 @@ type Modification struct {
 	NeverExpires bool       `xml:"never-expires,omitempty"`
 	Quantity     int        `xml:"quantity,omitempty"`
 	UpdatedAt    *time.Time `xml:"updated_at,omitempty"`
-}
-
-type ModificationRequest struct {
-	Amount                *Decimal            `xml:"amount,omitempty"`
-	NumberOfBillingCycles *nullable.NullInt64 `xml:"number-of-billing-cycles,omitempty"`
-	Quantity              int                 `xml:"quantity,omitempty"`
-	NeverExpires          bool                `xml:"never-expires,omitempty"`
-}
-
-type AddModificationRequest struct {
-	ModificationRequest
-	InheritedFromId string `xml:"inheritedFromId,omitempty"`
-}
-
-type UpdateModificationRequest struct {
-	ModificationRequest
-	ExistingId string `xml:"existingId,omitempty"`
-}
-
-type ModificationsRequest struct {
-	Add struct {
-		Type string                   `xml:"type,attr"`
-		Add  []AddModificationRequest `xml:"modification,omitempty"`
-	} `xml:"add"`
-	Update struct {
-		Type   string                      `xml:"type,attr"`
-		Update []UpdateModificationRequest `xml:"modification,omitempty"`
-	} `xml:"update"`
-	Remove struct {
-		Type   string `xml:"type,attr"`
-		Remove []string
-	} `xml:"remove"`
 }
