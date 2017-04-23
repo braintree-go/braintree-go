7bb59e1b5d00b7dc461d51079004871fc65ec0d4
Leigh McCulloch <leigh@mcchouse.com>
2017-04-23 06:57:23 +0000

    Remove use of testing subtests.
    What
    ===
    Remove use of testing subtests.
    
    Why
    ===
    Testing subtests were added in Go 1.7 which makes the build break for Go
    1.6 or earlier. Using them in these tests was beneficial because it
    allowed me to run the table driven test cases in parallel, but overall
    it's better we have the package pass across all versions of Go.

--- braintree_test.go
+++ braintree_test.go
@@ -1,7 +1,6 @@
 package braintree
 
 import (
-	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"strings"
@@ -20,44 +19,37 @@ func TestHttpClientTimeout(t *testing.T) {
 	env := NewEnvironment(server.URL)
 
 	testCases := []struct {
-		Description     string
 		Braintree       *Braintree
 		ExpectedTimeout time.Duration
 	}{
 		{
-			Description:     "Default Client",
 			Braintree:       New(env, "mid", "pubkey", "privkey"),
 			ExpectedTimeout: time.Second * 60,
 		},
 		{
-			Description:     "Custom Client",
 			Braintree:       NewWithHttpClient(env, "mid", "pubkey", "privkey", &http.Client{Timeout: time.Second * 10}),
 			ExpectedTimeout: time.Second * 10,
 		},
 	}
 
 	for _, tc := range testCases {
-		tc := tc
-		t.Run(fmt.Sprintf("%s ExpectedTimeout: %v", tc.Description, tc.ExpectedTimeout), func(t *testing.T) {
-			t.Parallel()
-			finished := make(chan bool)
-			go func() {
-				_, err := tc.Braintree.Transaction().Create(&TransactionRequest{})
-				if err == nil {
-					t.Fatal("Expected timeout error, received no error")
-				}
-				if !strings.Contains(err.Error(), "Timeout") {
-					t.Fatalf("Expected timeout error, received: %s", err)
-				}
-				finished <- true
-			}()
-
-			select {
-			case <-finished:
-				t.Logf("Timeout received as expected")
-			case <-time.After(tc.ExpectedTimeout + gracePeriod):
-				t.Fatalf("Timeout did not occur around %s, has been at least %s", tc.ExpectedTimeout, tc.ExpectedTimeout+gracePeriod)
+		finished := make(chan bool)
+		go func() {
+			_, err := tc.Braintree.Transaction().Create(&TransactionRequest{})
+			if err == nil {
+				t.Fatal("Expected timeout error, received no error")
 			}
-		})
+			if !strings.Contains(err.Error(), "Timeout") {
+				t.Fatalf("Expected timeout error, received: %s", err)
+			}
+			finished <- true
+		}()
+
+		select {
+		case <-finished:
+			t.Logf("Timeout received as expected")
+		case <-time.After(tc.ExpectedTimeout + gracePeriod):
+			t.Fatalf("Timeout did not occur around %s, has been at least %s", tc.ExpectedTimeout, tc.ExpectedTimeout+gracePeriod)
+		}
 	}
 }
