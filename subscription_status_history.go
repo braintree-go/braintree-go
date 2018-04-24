dc68ab3d906651a516adc854ea9716b9640fc28f
Silvan Jegen <silvan@smallpdf.com>
2018-04-24 09:51:35 +0200

    Use the SubscriptionStatus type instead of string

--- subscription_status_history.go
+++ subscription_status_history.go
@@ -13,12 +13,12 @@ type SubscriptionStatusHistory struct {
 // SubscriptionStatusEvent contains information about what and when
 // something changed about the subscription.
 type SubscriptionStatusEvent struct {
-	Timestamp          time.Time `xml:"timestamp"`
-	Balance            *Decimal  `xml:"balance"`
-	Price              *Decimal  `xml:"price"`
-	Status             string    `xml:"status"`
-	CurrencyISOCode    string    `xml:"currency-iso-code"`
-	User               string    `xml:"user"`
-	PlanID             string    `xml:"plan-id"`
-	SubscriptionSource string    `xml:"subscription-source"`
+	Timestamp          time.Time          `xml:"timestamp"`
+	Balance            *Decimal           `xml:"balance"`
+	Price              *Decimal           `xml:"price"`
+	Status             SubscriptionStatus `xml:"status"`
+	CurrencyISOCode    string             `xml:"currency-iso-code"`
+	User               string             `xml:"user"`
+	PlanID             string             `xml:"plan-id"`
+	SubscriptionSource string             `xml:"subscription-source"`
 }
