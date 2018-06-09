981ce7519f3ff38f307625e744d776280dc296eb
Leigh McCulloch <leigh@mcchouse.com>
2018-06-09 02:16:55 +0000

    Reorder gateway functions

--- dispute_gateway.go
+++ dispute_gateway.go
@@ -8,6 +8,18 @@ type DisputeGateway struct {
 	*Braintree
 }
 
+func (g *DisputeGateway) Find(ctx context.Context, disputeID string) (*Dispute, error) {
+	resp, err := g.executeVersion(ctx, "GET", "disputes/"+disputeID, nil, apiVersion4)
+	if err != nil {
+		return nil, err
+	}
+	switch resp.StatusCode {
+	case 200:
+		return resp.dispute()
+	}
+	return nil, &invalidResponseError{resp}
+}
+
 func (g *DisputeGateway) AddTextEvidence(ctx context.Context, disputeID string, textEvidenceRequest *DisputeTextEvidenceRequest) (*DisputeEvidence, error) {
 	resp, err := g.executeVersion(ctx, "POST", "disputes/"+disputeID+"/evidence", textEvidenceRequest, apiVersion4)
 	if err != nil {
@@ -20,16 +32,16 @@ func (g *DisputeGateway) AddTextEvidence(ctx context.Context, disputeID string, 
 	return nil, &invalidResponseError{resp}
 }
 
-func (g *DisputeGateway) Find(ctx context.Context, disputeID string) (*Dispute, error) {
-	resp, err := g.executeVersion(ctx, "GET", "disputes/"+disputeID, nil, apiVersion4)
+func (g *DisputeGateway) RemoveEvidence(ctx context.Context, disputeID string, evidenceId string) error {
+	resp, err := g.executeVersion(ctx, "DELETE", "disputes/"+disputeID+"/evidence/"+evidenceId, nil, apiVersion4)
 	if err != nil {
-		return nil, err
+		return err
 	}
 	switch resp.StatusCode {
 	case 200:
-		return resp.dispute()
+		return nil
 	}
-	return nil, &invalidResponseError{resp}
+	return &invalidResponseError{resp}
 }
 
 func (g *DisputeGateway) Accept(ctx context.Context, disputeID string) error {
@@ -46,18 +58,6 @@ func (g *DisputeGateway) Accept(ctx context.Context, disputeID string) error {
 
 func (g *DisputeGateway) Finalize(ctx context.Context, disputeID string) error {
 	resp, err := g.executeVersion(ctx, "PUT", "disputes/"+disputeID+"/finalize", nil, apiVersion4)
-	if err != nil {
-		return err
-	}
-	switch resp.StatusCode {
-	case 200:
-		return nil
-	}
-	return &invalidResponseError{resp}
-}
-
-func (g *DisputeGateway) RemoveEvidence(ctx context.Context, disputeID string, evidenceId string) error {
-	resp, err := g.executeVersion(ctx, "DELETE", "disputes/"+disputeID+"/evidence/"+evidenceId, nil, apiVersion4)
 	if err != nil {
 		return err
 	}
