79bfb7a0a5b1dc2922ddf856c0d4771a86deeb8e
Jose Valecillos <valecillosjg@gmail.com>
2018-10-01 17:57:34 +0200

    Adding bin field in ApplePayCard

--- apple_pay_card.go
+++ apple_pay_card.go
@@ -12,6 +12,7 @@ type ApplePayCard struct {
 	CardType              string         `xml:"card-type"`
 	PaymentInstrumentName string         `xml:"payment-instrument-name"`
 	SourceDescription     string         `xml:"source-description"`
+	Bin                   string         `xml:"bin,omitempty"`
 	Last4                 string         `xml:"last-4"`
 	ExpirationMonth       string         `xml:"expiration-month"`
 	ExpirationYear        string         `xml:"expiration-year"`
