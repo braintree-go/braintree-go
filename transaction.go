6b98d564adffc9d7b2bf9fece857925c30c917cd
Leigh McCulloch <leigh@mcchouse.com>
2018-11-08 16:47:42 +0000

    Add ProcessorResponseType to Transaction
    What
    ===
    Add ProcessorResponseType to Transaction.
    
    Why
    ===
    The official SDKs now expose a ProcessorResponseType field on
    Transactions that indicates of a processor response is approved, soft
    declined or hard declined.
    
    https://developers.braintreepayments.com/reference/response/transaction#processor_response_type
    
    Fixes #227

--- transaction.go
+++ transaction.go
@@ -77,6 +77,7 @@ type Transaction struct {
 	RefundedTransactionId        *string                   `xml:"refunded-transaction-id"`
 	ProcessorResponseCode        ProcessorResponseCode     `xml:"processor-response-code"`
 	ProcessorResponseText        string                    `xml:"processor-response-text"`
+	ProcessorResponseType        ProcessorResponseType     `xml:"processor-response-type"`
 	ProcessorAuthorizationCode   string                    `xml:"processor-authorization-code"`
 	SettlementBatchId            string                    `xml:"settlement-batch-id"`
 	EscrowStatus                 EscrowStatus              `xml:"escrow-status"`
