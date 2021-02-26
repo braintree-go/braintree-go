d456509015205553f69a39205bacbe3334cb9e64
Daniel Book <36259973+dbook13@users.noreply.github.com>
2021-02-26 12:12:15 -0700

    Add apple pay card marshal test

---
+++ apple_pay_card_test.go
@@ -0,0 +1,30 @@
+// +build unit
+
+package braintree
+
+import (
+	"encoding/xml"
+	"testing"
+)
+
+func TestApplePayCard_MarshalXML(t *testing.T) {
+	card := ApplePayCard{
+		ExpirationMonth: "10",
+		ExpirationYear:  "22",
+		ECI:             "07",
+		Cryptogram:      "testCardCryptogram",
+		Number:          "4111111111111111",
+		CardholderName:  "Test User",
+	}
+
+	cardString, err := xml.Marshal(card)
+	if err != nil {
+		t.Fatalf("%v", err)
+	}
+
+	expected := `<apple-pay-card><expiration-month>10</expiration-month><expiration-year>22</expiration-year><eci-indicator>07</eci-indicator><cryptogram>testCardCryptogram</cryptogram><number>4111111111111111</number><cardholder-name>Test User</cardholder-name></android-pay-card>`
+
+	if cardString != expected {
+		t.Errorf("Marshalled xml got [%s], want [%s]", cardString, expected)
+	}
+}
